d26e3f8c640f19222a94b4195f7842232bba3f03
VincenzoLivigni <enzolivigni13@gmail.com>
2025-11-26 12:46:51 +0100

    connect post paths to the controller

--- routes/posts.js
+++ routes/posts.js
@@ -1,34 +1,24 @@
-const express = require("express")                                       // importo modulo express
-const router = express.Router()                                          // creo variabile router per definire le rotte dei post
+const express = require("express")                                      // importo modulo express
+const router = express.Router()                                         // creo variabile router per definire le rotte dei post
+
+const postsController = require("../controllers/postsController")       // importo le funzioni dichiarate nel controller
 
 // index                                                                // mostra la lista di post
-router.get("/", (req, res) => {
-    res.send("Show all posts")
-})
+router.get("/", postsController.index)
 
 // show                                                                 // mostra uno specifico post
-router.get("/:id", (req, res) => {
-    res.send(`Show the post with id: ${req.params.id}`)
-})
+router.get("/:id", postsController.show)
 
 // store                                                                // crea un nuovo post
-router.post("/", (req, res) => {
-    res.send("Store a new post")
-})
+router.post("/", postsController.store)
 
 // update                                                               // modifica un post interamente
-router.put("/:id", (req, res) => {
-    res.send(`Update the post with id: ${req.params.id}`)
-})
+router.put("/:id", postsController.update)
 
 // modify                                                               // modifica un solo elemento del post
-router.patch("/:id", (req, res) => {
-    res.send(`Modify the post with id: ${req.params.id}`)
-})
+router.patch("/:id", postsController.modify)
 
 // destroy                                                              // elimina post
-router.delete("/:id", (req, res) => {
-    res.send(`Delete the post with id: ${req.params.id}`)
-})
+router.delete("/:id", postsController.destroy)
 
 module.exports = router                                                 // esporto il router
